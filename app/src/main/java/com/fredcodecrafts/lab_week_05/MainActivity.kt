3f7755a23f7dc852525bd763d79fa945f3433d93
Frederick Garner Wibowo <frederickgarner06@gmail.com>
2025-09-29 10:00:52 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/src/main/java/com/fredcodecrafts/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/fredcodecrafts/lab_week_05/MainActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.fredcodecrafts.lab_week_05.api.CatApiService
+import com.fredcodecrafts.lab_week_05.model.ImageData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import com.fredcodecrafts.lab_week_05.api.CatApiService
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +20,10 @@ class MainActivity : AppCompatActivity() {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
+
 
     // Cat API service
     private val catApiService by lazy {
@@ -43,14 +45,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-        call.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String>, t: Throwable) {
+        call.enqueue(object : Callback<List<ImageData>> {
+            override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+            override fun onResponse(call: Call<List<ImageData>>, response: Response<List<ImageData>>) {
                 if (response.isSuccessful) {
-                    apiResponseView.text = response.body()
+                    val image = response.body()
+                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    apiResponseView.text = getString(R.string.image_placeholder, firstImage)
                 } else {
                     Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
                             response.errorBody()?.string().orEmpty()
@@ -59,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
 
     companion object {
         const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
